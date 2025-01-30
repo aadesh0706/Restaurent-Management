6c47a2b64ddb470d7182ab05ceb341f6fabe2097
aadesh0706 <aadeshgulumbe3@gmail.com>
2025-01-29 23:34:24 -0800

    new

--- backend/server.js
+++ backend/server.js
@@ -21,9 +21,11 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
+app.get('/',(req,res)=>{res.send("Hello World")});
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/orders", ordersRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
